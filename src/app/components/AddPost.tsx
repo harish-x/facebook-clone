13ac61e7ecce9475cbe09733c8ed18e25b4028eb
harish-x <thalaharish2003@gmail.com>
2024-10-14 15:45:34 +0530

    images added

--- src/app/components/AddPost.tsx
+++ src/app/components/AddPost.tsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const AddPost = () => {
   return (
-    <div>
-      AddPost
-    </div>
-  )
-}
+    <div className="p-4 bg-white rounded-lg flex gap-4 justify-between text-sm ">
+      {/* avatar */}
+      <Image
+        src="https://images.pexels.com/photos/28874283/pexels-photo-28874283/free-photo-of-minimal-workspace-with-laptop-and-coffee-mug.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt=""
+        width={48}
+        height={48}
+        className="w-12 h-12 object-cover rounded-full"
+      />
+      {/* post */}
+      <div className="flex-1 ">
+        {/* text input */}
+        <div className=" flex gap-4">
+          <textarea
+            name=""
+            id=""
+            placeholder="What's on your mind?"
+            className="bg-slate-50 rounded-lg p-2 flex-1"
+          ></textarea>
+          <Image
+            src="https://img.icons8.com/?size=100&id=JijINYOWFFDS&format=png&color=000000"
+            alt=""
+            width={20}
+            height={20}
+            className="w-5 h-5  cursor-pointer self-end"
+          />
+        </div>
+      </div>
 
-export default AddPost
+      {/* post options */}
+      <div className="flex items-center gap-4 mt-4 text-gray-400">
+        <div className="">
+          <Image
+            src="/addimage.png"
+            alt=""
+            width={20}
+            height={20}
+            className="w-5 h-5  cursor-pointer self-end"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddPost;
--- src/app/components/Stories.tsx
+++ src/app/components/Stories.tsx
@@ -1,11 +1,74 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const Stories = () => {
   return (
-    <div>
-      Stories
+    <div className="p-4 bg-white rounded shadow-md overflow-scroll text-sm scrollbar-hide">
+      <div className="flex gap-8 w-max">
+        {/* story */}
+        <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/267371/pexels-photo-267371.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={40}
+            height={40}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-thin">Harish</span>
+              </div>
+              <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/267371/pexels-photo-267371.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={40}
+            height={40}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-thin">Harish</span>
+              </div>
+              <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/267371/pexels-photo-267371.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={40}
+            height={40}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-thin">Harish</span>
+              </div>
+              <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/267371/pexels-photo-267371.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={40}
+            height={40}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-thin">Harish</span>
+              </div>
+              <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/267371/pexels-photo-267371.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={40}
+            height={40}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-thin">Harish</span>
+              </div>
+              <div className="flex flex-col gap-2 items-center cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/267371/pexels-photo-267371.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={40}
+            height={40}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-thin">Harish</span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Stories
+export default Stories;
